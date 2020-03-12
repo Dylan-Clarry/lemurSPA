1db46590d4d90c03ffe1c8baa8213652cf85c1ab
dylan-clarry <dylan.imail@gmail.com>
2020-03-12 11:42:32 -0400

    fixed local url rewrites; no longer using hashchanges

--- app.js
+++ app.js
@@ -6,22 +6,6 @@ class LemurApp {
 		this.routes = {};
 		this.state = {};
 		this.content = '<h1>Test Header</h1>';
-
-		window.addEventListener('hashchange', _ => {
-
-			let pathName = location.hash.replace('#/', '');
-
-			console.log("hash has been changed!");
-			console.log("hash replace   :", pathName);
-			console.log("location href  :", window.location.href);
-			console.log("location origin:", window.location.origin);
-
-			window.history.pushState(
-				{},
-				pathName,
-				window.location.origin + pathName
-			);
-		});
 	}
 
 	run() {
@@ -30,6 +14,31 @@ class LemurApp {
 		const app = null || document.getElementById('app');
 		if(app) {
 			app.innerHTML = this.content;
+
+			let spaLinks = getSpaLinks();
+			console.log(spaLinks);
+			
+			for(let i = 0; i < spaLinks.length; i++) {
+				let currLink = spaLinks[i]
+				console.log(currLink);
+				
+				let href = currLink.getAttribute('href');
+
+				currLink.addEventListener("click", function(){
+
+					// prevent default a tag redirect
+					window.event.preventDefault();
+					let href = currLink.getAttribute('href')
+
+					console.log(href);
+					window.history.pushState(
+						{},
+						href,
+						window.location.origin + '' + href
+					);
+				}, false);
+			}
+
 		} else {
 			console.error("Lemur Error: root element div with id 'app' not found");
 		}
@@ -40,6 +49,33 @@ class LemurApp {
 	}
 }
 
-module.exports = {
-	LemurApp,
+let handleRedirect = _ => {
+	window.addEventListener('hashchange', _ => {
+
+		let pathName = location.hash.replace('#/', '');
+
+		console.log("hash has been changed!");
+		console.log("hash replace     :", pathName);
+		console.log("location href    :", window.location.href);
+		console.log("location origin  :", window.location.origin);
+
+		window.history.pushState(
+			{},
+			pathName,
+			window.location.origin + '/' + pathName
+		);
+	});
 }
+
+let getSpaLinks = _ => {
+	let spaLinks = document.querySelectorAll(".spalink");
+	return spaLinks
+}
+
+module.exports = { LemurApp };
+
+// a = document.getElementById("new_link");
+// a.addEventListener("click",function(){
+//      alert("preform action");
+//      window.event.preventDefault();
+// },false);
